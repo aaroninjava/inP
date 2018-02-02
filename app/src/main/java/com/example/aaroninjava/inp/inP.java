19256ad30636f752ef65749b58ce180805e56c09
aarontsai <aaroninpcschool241@gmail.com>
2018-02-02 09:33:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aaroninjava/inp/inP.java
@@ -0,0 +1,13 @@
+package com.example.aaroninjava.inp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_p);
+    }
+}
